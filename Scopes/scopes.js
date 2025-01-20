51dfe42d315036b343134bb67e803b903976aab2
Kalamadi Rajakumar Prakash <118983558+RajakumarKalamadi@users.noreply.github.com>
2025-01-21 01:47:07 +0530

    Scopes added

---
+++ Scopes/scopes.js
@@ -0,0 +1,49 @@
+// var teacher = "Sanket Singh";
+// function fun(){
+//     teacher = "Gaurav";
+//     content = "JS";
+//     console.log("Wow ", teacher, content);
+// }
+
+// fun();
+// console.log(teacher, content);
+
+
+// function fun(){
+//     console.log(x);
+//     var x = 10;
+//     console.log(x);
+    
+    
+// }
+
+// fun();
+
+
+// let x = 10; //block scope in the global area
+// console.log(x);
+// let x = 20; 
+// console.log(x);
+// function fun(x){
+
+//     try{
+//         let ans;
+//         if(x % 2 == 0){
+//           ans =  x / 0;
+//           return ans;
+//         }
+//     } catch (err){
+//         console.log("Hello raju");
+//         return err;
+        
+//     }
+// }
+// console.log(fun(10));
+
+
+const myFun = function(x){
+    console.log("Function expression :", x);
+    
+}
+
+myFun(10);
